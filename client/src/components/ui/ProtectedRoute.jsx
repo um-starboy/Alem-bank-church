150fb79c648508765e43e32377214096c93da87e
um-starboy <mrlonewolfyb@gmail.com>
2026-06-06 17:54:27 +0300

    restore: old long scrolling church website design from backup

--- client/src/components/ui/ProtectedRoute.jsx
+++ client/src/components/ui/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'
-import LoadingScreen from '../components/ui/LoadingScreen'
+import { useAuth } from '../../context/AuthContext'
+import LoadingScreen from './LoadingScreen'
 
 const ProtectedRoute = ({ children }) => {
   const { isLoggedIn, loading } = useAuth()
